cd5e63084cea4b828d733beec5df4c07ad919bec
yikolemon <duanfuqiang@eccom.com.cn>
2023-09-08 18:02:27 +0800

    高级函数

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { Component } from 'react';
 
 const list = [
   {
@@ -18,21 +18,68 @@ const list = [
     objectID: 1,
   },
 ];
-function App() {
-  return (
-    <div className='App'>
-      {list.map(item=>
+
+
+
+const isSearched=searchTerm=>(item)=>{
+  return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+}
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      list: list,
+      searchTerm: ''
+    };
+  }
+
+  //定义函数用箭头函数，不需要绑定
+  onDismiss=(id)=>{
+    const isNotId = item => item.objectID!==id;
+    const updateList=this.state.list.filter(isNotId);
+    this.setState({list:updateList})
+  }
+
+  onSearchChange=(event)=>{
+    this.setState({searchTerm: event.target.value});
+    console.log(event.target.value);
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <form>
+          <input 
+          type="text" 
+          onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>
           <div key={item.objectID}>
             <span>
-              <a href={item.url}>{item.title}</a>
+              <a href={item.url}>
+                {item.title}
+              </a>
             </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button 
+              onClick={()=>{
+                  this.onDismiss(item.objectID)
+              }}
+              type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
