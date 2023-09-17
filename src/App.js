36e7b500bc910c1444ef379846ced2d4d3a350dc
zko0 <1142496307@qq.com>
2023-09-17 21:22:28 +0800

    api查询

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux'
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
 
 
-
-const isSearched = searchTerm => (item) => {
-  return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
+  }
+
+  setSearchTopStories=(result) =>{
+    this.setState({ result });
+  }
+
+  fetchSearchTopStories=(searchTerm)=>{
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(e => e);
+  }
+
+  componentDidMount=()=>{
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   //定义函数用箭头函数，不需要绑定
   onDismiss = (id) => {
     const isNotId = item => item.objectID !== id;
-    const updateList = this.state.list.filter(isNotId);
-    this.setState({ list: updateList })
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: {...this.state.result, hits: updatedHits}
+    });
   }
 
   onSearchChange = (event) => {
@@ -47,43 +49,56 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+  onSearchSubmit=(event)=>{
+    const{searchTerm}=this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+
   render() {
-    const { list, searchTerm } = this.state;
+    const { result, searchTerm } = this.state;
+    //!result阻止了组件的显示
+
     return (
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          pattern={searchTerm}
+        {
+          result
+          && <Table
+          list={result.hits}
           onDismiss={this.onDismiss}
-        />
+          />
+        }
       </div>
     )
   }
 }
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children}
+
+const Search = ({ value, onChange, onSubmit,children}) =>
+  <form onSubmit={onSubmit}>
     <input
       type="text"
       onChange={onChange}
       value={value}
     />
+    <button type='submit'>
+      {children}
+    </button>
   </form>
 
-
-const Table=({list, pattern, onDismiss})=>{
+const Table=({list,onDismiss})=>{
   return (
     <div className="table">
       {
-        list.filter(isSearched(pattern)).map(item =>
+        list.map(item =>
           <div key={item.objectID} className="table-row">
             <span style={{width:'40%'}}>
               <a href={item.url}>
