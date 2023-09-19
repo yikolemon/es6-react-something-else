d979e081b1305d1edad0b9a594df67434409f717
zko0 <1142496307@qq.com>
2023-09-19 23:21:37 +0800

    分页与本地缓存

--- src/App.js
+++ src/App.js
@@ -2,29 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux'
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
 
+//const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}`;
+
+//console.log(url);
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: null,
-      searchTerm: DEFAULT_QUERY
+      results: null,
+      searchTerm: DEFAULT_QUERY,
+      searchKey:''
     };
   }
 
   setSearchTopStories=(result) =>{
-    this.setState({ result });
+    const {hits,page}= result;
+    const{searchKey,results}=this.state;
+    const oldHits = results && results[searchKey]
+    ? results[searchKey].hits
+    : [];
+
+    const updatedHits=[
+      ...oldHits,
+      ...hits
+    ]
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+        }
+    });
   }
 
-  fetchSearchTopStories=(searchTerm)=>{
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories=(searchTerm,page=0)=>{
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(e => e);
@@ -32,15 +54,24 @@ class App extends Component {
 
   componentDidMount=()=>{
     const { searchTerm } = this.state;
+    this.setState({searchKey: searchTerm});
     this.fetchSearchTopStories(searchTerm);
   }
 
   //定义函数用箭头函数，不需要绑定
   onDismiss = (id) => {
+    const{searchKey,results}=this.state;
+    const{hits,page}=results[searchKey];
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: {...this.state.result, hits: updatedHits}
+      results: {
+        ...results,
+        [searchKey]: {
+          hits:updatedHits,
+          page
+        }
+      }
     });
   }
 
@@ -51,13 +82,22 @@ class App extends Component {
 
   onSearchSubmit=(event)=>{
     const{searchTerm}=this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
     event.preventDefault();
   }
 
+  needsToSearchTopStories(searchTerm){
+    return !this.state.results[searchTerm];
+  }
+
   render() {
-    const { result, searchTerm } = this.state;
+    const { results, searchTerm,searchKey} = this.state;
     //!result阻止了组件的显示
+    const page = (results &&results[searchKey]&& results[searchKey].page) || 0;
+
 
     return (
       <div className="page">
@@ -71,12 +111,18 @@ class App extends Component {
           </Search>
         </div>
         {
-          result
+          results
+          &&results[searchKey]
           && <Table
-          list={result.hits}
+          list={results[searchKey].hits}
           onDismiss={this.onDismiss}
           />
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+          More
+          </Button>
+        </div>
       </div>
     )
   }
